307bed22c32999ebb7f7a45a1424df9ed300901f
Abdellah Ennajari <105159582+ennajari@users.noreply.github.com>
2024-07-10 23:15:26 +0100

    Update calculatrice.java

--- calculatrice.java
+++ calculatrice.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Bienvenue dans la calculatrice simple !");
         
         while (true) {
