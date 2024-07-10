47df0ae562a6a0b8eef8ac7a3e4534a3a959ae01
Abdellah Ennajari <105159582+ennajari@users.noreply.github.com>
2024-07-10 23:16:01 +0100

    Update calculatrice.java

--- calculatrice.java
+++ calculatrice.java
@@ -3,7 +3,6 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bienvenue dans la calculatrice simple !");
-
         while (true) {
             System.out.println("Choisissez une opération :");
             System.out.println("1. Addition");
